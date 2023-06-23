6ddb86793c6f8730a765b1f65458babde9f1b3a8
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2023-06-23 15:26:25 -0400

    hard coded path prefix "/pentaho/osgi" in data.service.js and helper.service.js - poc to test other parts of app, will revist

--- plugins/file-open-save-new-js/core/src/main/javascript/app/services/data.service.js
+++ plugins/file-open-save-new-js/core/src/main/javascript/app/services/data.service.js
@@ -360,7 +360,7 @@ define(
           } else {
             url += "?v=" + value;
           }
-          return url;
+          return "/pentaho/osgi"+ url; // FIXME find better solution
         }
       }
     });
