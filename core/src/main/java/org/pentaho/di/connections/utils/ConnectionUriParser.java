b48f6674325987ac4b8c74197571d8313ed7aa1b
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2024-05-15 15:40:22 -0400

    [POC] created ConnectionUriParser

---
+++ core/src/main/java/org/pentaho/di/connections/utils/ConnectionUriParser.java
@@ -0,0 +1,27 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2024 Hitachi Vantara. All rights reserved.
+ */
+
+package org.pentaho.di.connections.utils;
+
+/**
+ * Helper class to extract information from the
+ * {@value org.pentaho.di.connections.vfs.provider.ConnectionFileProvider#SCHEME} URI.
+ * This class has limited functionality. Main focus is a temporary substitute for {@link java.net.URI} due to
+ * special characters in the URI.
+ */
+public class ConnectionUriParser {
+}
